eba6b5c245c8c857c970378d19d9e5b07b35763c
Krishil Ghelani <77043031+Krishil-Ghelani@users.noreply.github.com>
2021-03-28 01:17:44 +0530

    locating the Virtual machine

---
+++ getting_ovm_location.py
@@ -0,0 +1,13 @@
+import pyvbox
+import virtualbox
+
+# virtualbox.import_vboxapi
+try:
+    vbox = virtualbox.VirtualBox()
+    print("VM(s):\n + %s" % "\n + ".join([vm.name for vm in vbox.machines]))
+except ModuleNotFoundError as e:
+    try:
+        vbox = virtualbox.VirtualBox()
+        print("VM(s):\n + %s" % "\n + ".join([vm.name for vm in vbox.machines]))
+    except ModuleNotFoundError as e:
+        pass
